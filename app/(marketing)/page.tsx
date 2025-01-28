91092ec06c68e0438b3b84581b64a7078b1fb163
Quentin <154846367+notro636@users.noreply.github.com>
2025-01-28 03:41:10 +0000

    Blumemail is now discontinued permanently. Find out why on the SparkMask Blog.

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -2,77 +2,22 @@
 
 import {
   Box,
-  ButtonGroup,
   Container,
-  Flex,
-  HStack,
-  Heading,
-  Icon,
-  IconButton,
-  Stack,
-  Tag,
-  Text,
-  VStack,
-  Wrap,
-  useClipboard,
+  Stack
 } from '@chakra-ui/react'
 import { Br, Link } from '@saas-ui/react'
-import type { Metadata, NextPage } from 'next'
-import Image from 'next/image'
-import {
-  FiArrowRight,
-  FiBox,
-  FiCheck,
-  FiCode,
-  FiCopy,
-  FiFlag,
-  FiGrid,
-  FiLock,
-  FiSearch,
-  FiSliders,
-  FiSmile,
-  FiTerminal,
-  FiThumbsDown,
-  FiThumbsUp,
-  FiToggleLeft,
-  FiTrendingUp,
-  FiUserPlus,
-} from 'react-icons/fi'
-
+import type { NextPage } from 'next'
 import * as React from 'react'
-
-import { ButtonLink } from '#components/button-link/button-link'
-import { Faq } from '#components/faq'
-import { Features } from '#components/features'
 import { BackgroundGradient } from '#components/gradients/background-gradient'
 import { Hero } from '#components/hero'
-import {
-  Highlights,
-  HighlightsItem,
-  HighlightsTestimonialItem,
-} from '#components/highlights'
-import { NextjsLogo } from '#components/logos'
 import { FallInPlace } from '#components/motion/fall-in-place'
-import { Pricing } from '#components/pricing/pricing'
-import { Testimonial, Testimonials } from '#components/testimonials'
-import { Em } from '#components/typography'
-import faq from '#data/faq'
-import pricing from '#data/pricing'
-import testimonials from '#data/testimonials'
 import { AnnouncementBanner } from '#components/announcement-banner'
-import { AiFillLock } from 'react-icons/ai'
 
 
 const Home: NextPage = () => {
   return (
     <Box>
       <HeroSection />
-
-      <TestimonialsSection />
-
-      <PricingSection />
-
-      { /* <FaqSection /> */ }
     </Box>
   )
 }
@@ -81,13 +26,13 @@ const HeroSection: React.FC = () => {
   return (
     <Box position="relative" overflow="hidden">
       <BackgroundGradient height="100%" zIndex="-1" />
-      <AnnouncementBanner title={'Blumemail is now discontinued. Learn More. 👉'} description={''} href={'https://blog.sparkmask.com/posts/blumemail-discontinued'} />
+      <AnnouncementBanner title={'This website is now archived.'} description={''} href={''} />
       <Container maxW="container.xl" pt={{ base: 40, lg: 60 }} pb="40">
         <Stack direction={{ base: 'column', lg: 'row' }} alignItems="center">
           
           <Hero
             id="home"
-            justifyContent="flex-start"
+            justifyContent="flex-middle"
             px="0"
             title={
               <FallInPlace>
@@ -101,109 +46,11 @@ const HeroSection: React.FC = () => {
               </FallInPlace>
             }
           >
-            <FallInPlace delay={0.8}>
-              <HStack pt="4" pb="12" spacing="8">
-                <Link href={'https://mailcow.email'} target='_blank'><Image src={'/static/images/mailcow.png'} alt={'mailcow Logo'} width={'28'} height={'28'}/></Link> <NextjsLogo height="20px" />
-              </HStack>
-
-              <ButtonGroup spacing={4} alignItems="center">
-                <ButtonLink colorScheme="primary" size="lg" href="#">
-                  <s>Get Blumemail</s>
-                </ButtonLink>
-              </ButtonGroup>
-            </FallInPlace>
           </Hero>
         </Stack>
       </Container>
-
-      <s> <Features
-        id="benefits"
-        columns={[1, 2, 4]}
-        iconSize={4}
-        innerWidth="container.xl"
-        pt="20"
-        features={[
-          {
-            title: 'Freedom of Choice',
-            icon: FiSmile,
-            description: 'Blumemail\'s webmail software will always support your choice of mail client.',
-            iconPosition: 'left',
-            delay: 0.6,
-          },
-          {
-            title: 'We Care.',
-            icon: FiThumbsUp,
-            description:
-              'Blumemail will always be there for you, your feedback always helps!',
-            iconPosition: 'left',
-            delay: 0.8,
-          },
-          {
-            title: 'Your email, your domain.',
-            icon: FiCode,
-            description:
-              'All Blumemail plans support adding your own domain, connect with us and we\'ll have it setup for you.',
-            iconPosition: 'left',
-            delay: 1,
-          },
-          {
-            title: 'Advertisments Be Gone!',
-            icon: FiLock,
-            description:
-              'Designed to protect your privacy, Blumemail will never gives you targeted ads.',
-            iconPosition: 'left',
-            delay: 1.1,
-          },
-        ]}
-        reveal={FallInPlace}
-      /> </s>
     </Box>
   )
-}
-
-const TestimonialsSection = () => {
-  const columns = React.useMemo(() => {
-    return testimonials.items.reduce<Array<typeof testimonials.items>>(
-      (columns, t, i) => {
-        columns[i % 3].push(t)
-
-        return columns
-      },
-      [[], [], []],
-    )
-  }, [])
-
-  return (
-    <Testimonials
-      title={testimonials.title}
-      columns={[1, 2, 3]}
-      innerWidth="container.xl"
-    >
-      <>
-        {columns.map((column, i) => (
-          <Stack key={i} spacing="8">
-            {column.map((t, i) => (
-              <Testimonial key={i} {...t} />
-            ))}
-          </Stack>
-        ))}
-      </>
-    </Testimonials>
-  )
-}
-
-const PricingSection = () => {
-  return (
-    <Pricing {...pricing}>
-      <Text p="8" textAlign="center" color="muted">
-        Payments are processed through Stripe.
-      </Text>
-    </Pricing>
-  )
-}
-
-const FaqSection = () => {
-  return <Faq {...faq} />
 }
 
 export default Home
